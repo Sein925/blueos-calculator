e708a9ef883ec8bc06adb453fe8ae9388192780b
Yikang666 <2749436915@qq.com>
2026-03-01 14:52:13 +0800

    feat: 添加设备信息获取功能并优化支付界面
    - 在manifest.json中添加设备信息权限和模块
    - 在global.js中实现设备ID获取功能
    - 替换微信支付图标为图片资源
    - 优化支付界面布局和样式
    - 移除设备ID显示卡片

--- src/global.js
+++ src/global.js
@@ -3,11 +3,20 @@
  * 如果需要增加全局变量，请同步更新 app.d.ts，可用于代码提示、错误检测
  */
 import router from '@blueos.app.appmanager.router'
+import device from '@blueos.hardware.deviceInfo'
 import fetch from '@blueos.network.fetch'
+
+device.getDeviceId({
+  success: function (data) {
+    global.deviceId = data.deviceId
+  },
+  fail: function (code) {
+    console.log(`handling fail, code = ${code}`)
+  },
+})
 
 global.router = router
 global.fetch = fetch.fetch
 
 global.calculatorHistory = []
 global.backendUrl = 'https://calculator.666-114514.eu.org'
-global.deviceId = 'default-device-id'
